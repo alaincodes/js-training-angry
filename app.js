28cf454d8e19f37b1581436974121e52e7e1dc54
Alain <alaint77@gmail.com>
2019-07-17 23:10:54 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const ROOT_EL = document.getElementById("root");
+const NAV_BAR = document.createElement("nav");
+
+NAV_BAR.style.backgroundColor = "red";
+NAV_BAR.style.height = "9vh";
+
+ROOT_EL.appendChild(NAV_BAR);
